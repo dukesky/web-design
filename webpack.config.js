32235f59d11aff1759b35acc47914356f2a04289
dukesky <36315007+dukesky@users.noreply.github.com>
2019-12-08 17:43:58 -0800

    update website

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path')
+const postCSSPlugins = [
+    require('postcss-simple-vars'),
+    require('postcss-nested'),
+    require('autoprefixer')
+ ]
+
+module.exports = {
+    entry: './app/assets/script/App.js',
+    output:{
+        filename: 'bundled.js',
+        path: path.resolve(__dirname,'app')
+    },
+
+    mode: 'development',
+    watch : true,
+    module: {
+        rules: [
+            {test: /\.css$/i,
+             use: ['style-loader', 'css-loader?url=false', {loader: 'postcss-loader', options: {plugins: postCSSPlugins}}]
+            }
+        ]
+    }
+}
